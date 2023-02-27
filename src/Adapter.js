42db34b08c373aa67181b796be72d70d2181a3f8
Najma <najma.mahat@student.moringaschool.com>
2023-02-27 19:42:48 +0300

    Completed assignment

--- src/Adapter.js
+++ src/Adapter.js
@@ -4,10 +4,11 @@ class Adapter {
     .then(res => res.json())
   }
 
-  // static getShowEpisodes (showID){
-  //   return fetch(`http://api.tvmaze.com/shows/${showID}/episodes`)
-  //   .then(res => res.json)
-  // }
+  static getShowEpisodes(showID) {
+    return fetch(`http://api.tvmaze.com/shows/${showID}/episodes`).then((res) =>
+      res.json()
+    );
+  }
 }
 
 export default Adapter
